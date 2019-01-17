d7e0a12d5c24faefaff166cee10badff793c211a
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-01-17 19:19:38 +0100

    Update lineChart.js

--- src/lineChart.js
+++ src/lineChart.js
@@ -4,53 +4,45 @@ import Chart from './chart'
 
 export default class LineChart extends Chart{
 
-  constructor (selector, config) {
-    super(selector, config)
-  }
-
-  // Set scales
-  setScales () {
-    super.setScales()
-    return this
-  }
-
   // Setup line renderer
   setRenderer() {
     this.lineRenderer = line()
       .defined(d => !isNaN(this.y(d)))
       .x(d => this.scaleX(this.x(d)))
       .y(d => this.scaleY(this.y(d)))
+    return this
   }
 
   // Render chart
   render () {
     super.render()
-
     // Render chart line
     this.line = this.chart.append('path')
       .datum(this.data)
       .attr('class', 'line')
       .attr('d', this.lineRenderer)
-
     return this
   }
 
   // Clear chart
   clear () {
     super.clear()
-
+    // Remove paths
     this.chart.selectAll('path').remove()
-
     return this
   }
 
   // Resize chart
   resize () {
     super.resize()
-
     // Update line path
     if (this.line) this.line.attr('d', this.lineRenderer)
+    return this
+  }
 
+  // Set renderer curve
+  curve (type) {
+    this.lineRenderer.curve(type)
     return this
   }
 }
