481a83a61a0ecdb561dc1f0fde48d32d0e4b7fc5
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-01-17 17:11:58 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,9 +1,8 @@
 import * as d3 from 'd3'
 
-export default class {
+export default class Test {
 
   constructor (selector) {
-    // Select chart container
     this.el = d3.select(selector)
     this.el.append('svg')
   }
