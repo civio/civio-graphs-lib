86b769513d6864d51af0286201e1e0bbb41ba5fc
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-01-17 13:31:17 +0100

    Update index & package

--- index.js
+++
@@ -1,3 +0,0 @@
-module.exports = function civio_graphs () {
-  console.log("This is Civio graphs lib");
-};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export Chart from "./chart";
+export LineChart from "./lineChart";
+export AreaChart from "./areaChart";
+export tooltip from "./tooltip";
