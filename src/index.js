63722d3cf50286d38682102d189b25310bf9d7fe
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-01-17 18:41:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export {default as Chart} from "./chart";
+export {default as AreaChart} from "./areaChart";
+export {default as LineChart} from "./lineChart";
+export {default as tooltip} from "./tooltip";
