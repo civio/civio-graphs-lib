77b955fcac4381417ec68b5554db8127dbd00ce5
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-01-17 15:53:18 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import {terser} from "rollup-plugin-terser";
+import * as meta from "./package.json";
+
+const config = {
+  input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}),
+  output: {
+    file: `dist/${meta.name}.js`,
+    name: meta.name,
+    format: "umd",
+    indent: false,
+    extend: true,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}))
+  },
+  plugins: []
+};
+
+export default [
+  config,
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: `dist/${meta.name}.min.js`
+    }
+  }
+];
