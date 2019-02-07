66135cf1cae393f60f26e86a7375aefcbf50f2b2
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2019-02-07 16:54:20 +0100

    Update dist

--- dist/civio-graphs-lib.js
+++ dist/civio-graphs-lib.js
@@ -1,34 +1,50 @@
 // civio-graphs-lib v0.1.3 Copyright 2019 Raúl Díaz Poblete
 (function (global, factory) {
-typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-array'), require('d3-selection'), require('lodash'), require('d3-axis'), require('d3-format'), require('d3-scale'), require('d3-time'), require('d3-time-format'), require('d3-shape'), require('slugify'), require('d3-interpolate'), require('d3-scale-chromatic'), require('d3-hierarchy')) :
-typeof define === 'function' && define.amd ? define(['exports', 'd3-array', 'd3-selection', 'lodash', 'd3-axis', 'd3-format', 'd3-scale', 'd3-time', 'd3-time-format', 'd3-shape', 'slugify', 'd3-interpolate', 'd3-scale-chromatic', 'd3-hierarchy'], factory) :
-(factory((global['civio-graphs-lib'] = global['civio-graphs-lib'] || {}),global.d3Array,global.d3Selection,global.lodash,global.d3Axis,global.d3Format,global.d3Scale,global.d3Time,global.d3TimeFormat,global.d3Shape,global.slugify,global.d3Interpolate,global.d3ScaleChromatic,global.d3Hierarchy));
-}(this, (function (exports,d3Array,d3Selection,lodash,d3Axis,d3Format,d3Scale,d3Time,d3TimeFormat,d3Shape,slugify,d3Interpolate,d3ScaleChromatic,d3Hierarchy) { 'use strict';
+typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-selection'), require('lodash'), require('d3-array'), require('d3-axis'), require('d3-format'), require('d3-scale'), require('d3-time'), require('d3-time-format'), require('d3-shape'), require('slugify'), require('d3-collection'), require('d3-interpolate'), require('d3-scale-chromatic'), require('d3-hierarchy')) :
+typeof define === 'function' && define.amd ? define(['exports', 'd3-selection', 'lodash', 'd3-array', 'd3-axis', 'd3-format', 'd3-scale', 'd3-time', 'd3-time-format', 'd3-shape', 'slugify', 'd3-collection', 'd3-interpolate', 'd3-scale-chromatic', 'd3-hierarchy'], factory) :
+(factory((global['civio-graphs-lib'] = global['civio-graphs-lib'] || {}),global.d3Selection,global.lodash,global.d3Array,global.d3Axis,global.d3Format,global.d3Scale,global.d3Time,global.d3TimeFormat,global.d3Shape,global.slugify,global.d3Collection,global.d3Interpolate,global.d3ScaleChromatic,global.d3Hierarchy));
+}(this, (function (exports,d3Selection,lodash,d3Array,d3Axis,d3Format,d3Scale,d3Time,d3TimeFormat,d3Shape,slugify,d3Collection,d3Interpolate,d3ScaleChromatic,d3Hierarchy) { 'use strict';
 
 slugify = slugify && slugify.hasOwnProperty('default') ? slugify['default'] : slugify;
 
 const configDefaults = {
-  // we can define an aspectRatio to calculate height or define a fixed height
   point: false,
-  align: true,
+  align: 'auto', // auto | center | left | right
+  valign: 'top', // top | bottom
   background: false
 };
 
 class Tooltip {
   constructor(_chart, config) {
     this.chart = _chart;
-    // get tooltip element from chart container
-    this.el = this.chart.el.select('.tooltip');
     // Setup config object
     this.config = lodash.defaults(config, configDefaults);
+    // Setup tooltip markup
+    this.setup();
+  }
+
+  setup() {
+    // append tooltip element to chart container
+    this.setupElement();
+    // append labels to tooltip element
+    this.setupLabels();
+    return this
+  }
+
+  setupElement() {
+    this.el = this.chart.el
+      .append('div')
+      .attr('class', this.config.background ? 'tooltip tooltip-bkg' : 'tooltip');
+    return this
+  }
+
+  setupLabels() {
+    this.el.append('div').attr('class', 'label x');
+    this.el.append('div').attr('class', 'label y');
     return this
   }
 
   render() {
-    // Skip if there is no tooltip element
-    if (!this.el || this.el.empty()) return this
-    // Setup bisectDate for mouse move
-    this.bisectDate = d3Array.bisector(this.chart.x).left;
     // Setup chart point
     if (this.config.point) {
       this.point = this.chart.chart
@@ -59,43 +75,79 @@ class Tooltip {
     return this
   }
 
-  setPosition(dataX) {
-    const { x, y } = this.getPosition(dataX);
-    // set point position
-    if (this.point) this.point.attr('transform', `translate(${x}, ${y})`);
-    // set tooltip position
-    this.el.style('top', `${y}px`);
-    if (this.config.align) {
-      const isRight = x > this.chart.width / 2;
-      this.el
-        .style('left', isRight ? 'auto' : `${x}px`)
-        .style('right', isRight ? `${this.chart.width - x}px` : 'auto')
-        .classed('right', isRight);
-    } else {
-      this.el.style('left', `${x}px`);
-    }
+  setContent(data) {
+    // Set x & y labels
+    this.setLabel('x', this.chart.tooltipFormatX()(this.chart.x(data)));
+    this.setLabel('y', this.chart.tooltipFormatY()(this.chart.y(data)));
     return this
   }
 
-  setContent(x, y) {
-    // Set label x value
-    this.el.select('.label.x').html(this.chart.tooltipFormatX()(x));
-    // Set label y value
-    this.el.select('.label.y').html(this.chart.tooltipFormatY()(y));
+  setLabel(selector, data, show = null) {
+    this.el.select(`.label.${selector}`).html(data);
+    // show/hide label
+    if (show !== null)
+      this.el.select(`.label.${selector}`).style('opacity', show ? 1 : 0);
+    return this
+  }
+
+  setPosition(dataX) {
+    const { x, y } = this.chart.getDataPosition(dataX);
+    // set point position
+    if (this.point) this.point.attr('transform', `translate(${x}, ${y})`);
+    // set x position
+    this.setPositionX(
+      x,
+      this.config.align !== 'auto'
+        ? this.config.align
+        : x > this.chart.width / 2
+        ? 'right'
+        : 'left'
+    );
+    // set y position
+    this.setPositionY(y, this.config.valign);
+    return this
+  }
+
+  setPositionX(x, align) {
+    if (align === 'left') {
+      this.el
+        .style('left', `${x}px`)
+        .style('right', 'auto')
+        .classed('right', false);
+    } else if (align === 'right') {
+      this.el
+        .style('right', `${this.chart.width - x}px`)
+        .style('left', 'auto')
+        .classed('right', true);
+    } else if (align === 'center') {
+      this.el
+        .style(
+          'left',
+          `${x - this.el.node().getBoundingClientRect().width / 2}px`
+        )
+        .classed('center', true);
+    }
+  }
+
+  setPositionY(y, align) {
+    if (align === 'top') {
+      this.el.style('top', `${y}px`);
+    } else if (align === 'bottom') {
+      this.el.style('bottom', `${this.chart.height - y}px`);
+    }
   }
 
   onMouseMove() {
     // get current data by mouse position
-    const d = this.getMouseData(d3Selection.event.layerX);
+    const d = this.chart.getMouseData(d3Selection.event.layerX);
     if (this.currentData !== d) {
       this.currentData = d;
+      // set tooltip content
+      this.setContent(this.currentData);
       // set tooltip position
       this.setPosition(this.currentData);
-      // set tooltip label
-      this.setContent(
-        this.chart.x(this.currentData),
-        this.chart.y(this.currentData)
-      );
+      // highlight current item in chart
+      this.chart.highlight(this.currentData);
     }
     return this
   }
@@ -109,30 +161,11 @@ class Tooltip {
   onMouseLeave() {
     // hide tooltip element & point
     this.hide();
+    // clear highlighted item in chart
+    this.chart.highlight(null);
+    // clear current data
+    this.currentData = null;
     return this
-  }
-
-  getPosition(data) {
-    // get data position for current date
-    const i = this.bisectDate(this.chart.data, this.chart.x(data));
-    const d =
-      i < this.chart.data.length
-        ? this.chart.data[i]
-        : this.chart.data[this.chart.data.length - 1];
-    return {
-      x: this.chart.scaleX(this.chart.x(d)),
-      y: this.chart.scaleY(this.chart.y(d))
-    }
-  }
-
-  getMouseData(x) {
-    // get mouse position
-    const mouseX = this.chart.scaleX.invert(x);
-    // calculate current data
-    const i = this.bisectDate(this.chart.data, mouseX, 1);
-    const d0 = this.chart.data[i - 1];
-    const d1 = this.chart.data[i];
-    return d1 && mouseX - this.chart.x(d0) > this.chart.x(d1) - mouseX ? d1 : d0
   }
 }
 
@@ -150,7 +183,8 @@ const configDefaults$1 = {
   axis: {
     x: true,
     y: true
-  }
+  },
+  tooltip: true
 };
 
 class Chart {
@@ -165,9 +199,8 @@ class Chart {
     // Setup config object
     this.config = lodash.defaultsDeep(config, configDefaults$1);
     this.width = 0;
-    // Set chart & tooltip
+    // Set chart
     this.setChart();
-    this.setTooltip();
   }
 
   setChart() {
@@ -175,8 +208,19 @@ class Chart {
     return this
   }
 
-  setTooltip() {
-    this.tooltip = new Tooltip(this);
+  setup(data) {
+    this.setData(data);
+    this.setFormat();
+    this.onResize();
+    this.setScales();
+    this.setAxis();
+    this.setRenderer();
+    if (this.config.tooltip) this.setTooltip();
+    return this
+  }
+
+  setData(data) {
+    this.data = data;
   }
 
   // Set formats
@@ -247,6 +291,8 @@ class Chart {
       .domain(this.scaleYDomain())
       .nice()
       .range(this.scaleYRange());
+    // bisector for tooltip data & position
+    this.bisector = d3Array.bisector(this.x).left;
     return this
   }
 
@@ -299,14 +345,8 @@ class Chart {
     return this
   }
 
-  setup(data) {
-    this.data = data;
-    this.setFormat();
-    this.onResize();
-    this.setScales();
-    this.setAxis();
-    this.setRenderer();
-    return this
+  setTooltip() {
+    this.tooltip = new Tooltip(this);
   }
 
   // Render chart based on data
@@ -317,7 +357,7 @@ class Chart {
     if (this.config.axis.y) {
       this.chart.append('g').call(this.axisY);
     }
-    this.tooltip.render();
+    if (this.tooltip) this.tooltip.render();
     this.setResize();
     return this
   }
@@ -399,7 +439,7 @@ class Chart {
     return d3TimeFormat.timeFormat(this.config.lang === 'es' ? '%d %B, %Y' : '%B %d, %Y')
   }
   tooltipFormatY() {
-    return d3Format.format('$,.1f')
+    return d3Format.format('$,d')
   }
   currencyFormat() {
     return ['', '\u00a0€']
@@ -428,15 +468,37 @@ class Chart {
   scaleYRange() {
     return [this.height - this.config.margin.bottom, this.config.margin.top]
   }
+
+  // Tooltip data for a mouse position
+  getMouseData(x) {
+    // get mouse position
+    const mouseX = this.scaleX.invert(x);
+    // calculate current data
+    const i = this.bisector(this.data, mouseX, 1);
+    const d0 = this.data[i - 1];
+    const d1 = this.data[i];
+    return d1 && mouseX - this.x(d0) > this.x(d1) - mouseX ? d1 : d0
+  }
+
+  // Tooltip position for a given data
+  getDataPosition(data) {
+    // get data position for current date
+    const i = this.bisector(this.data, this.x(data));
+    const d =
+      i < this.data.length ? this.data[i] : this.data[this.data.length - 1];
+    return {
+      x: this.scaleX(this.x(d)),
+      y: this.scaleY(this.y(d))
+    }
+  }
+
+  // Mouse over chart highlight
+  highlight() {
+    return this
+  }
 }
 
 class LineChart extends Chart {
-  setTooltip() {
-    this.tooltip = new Tooltip(this, {
-      point: true
-    });
-  }
-
   // Setup line renderer
   setRenderer() {
     this.lineRenderer = d3Shape.line()
@@ -444,6 +506,12 @@ class LineChart extends Chart {
       .x(d => this.scaleX(this.x(d)))
       .y(d => this.scaleY(this.y(d)));
     return this
+  }
+
+  setTooltip() {
+    this.tooltip = new Tooltip(this, {
+      point: true
+    });
   }
 
   // Render chart
@@ -539,11 +607,6 @@ class AreaChart extends LineChart {
 }
 
 class BarVerticalChart extends Chart {
-  setTooltip() {
-    this.tooltip = new Tooltip(this, {
-      align: false
-    });
-  }
   // Set scales
   setScales() {
     super.setScales();
@@ -555,6 +618,13 @@ class BarVerticalChart extends Chart {
       .range(this.scaleX.domain());
     this.scaleX.invert = x => this.scaleXInvert(x);
     return this
+  }
+
+  setTooltip() {
+    this.tooltip = new Tooltip(this, {
+      align: 'center',
+      valign: 'bottom'
+    });
   }
 
   // Render chart
@@ -630,6 +700,36 @@ class BarVerticalChart extends Chart {
     return this.data.map(this.x)
   }
 
+  // Tooltip position for a given data
+  getDataPosition(data) {
+    // get data position for current date
+    const i = this.bisector(this.data, this.x(data));
+    const d =
+      i < this.data.length ? this.data[i] : this.data[this.data.length - 1];
+    return {
+      x: this.scaleX(this.x(d)) + this.scaleX.bandwidth() / 2,
+      y: this.scaleY(this.y(d))
+    }
+  }
+
+  // Mouse over chart highlight
+  highlight(data) {
+    // clear highlighted x axis labels
+    this.el.selectAll('.axis.x text').classed('active', false);
+    // clear highlighted bars
+    this.el.selectAll('.bar').classed('active', false);
+    if (data) {
+      // highlight current x axis label
+      this.el
+        .selectAll('.axis.x text')
+        .filter(d => d === this.x(data))
+        .classed('active', true);
+      // highlight current bar
+      this.el.select(`.bar-${this.x(data)}`).classed('active', true);
+    }
+    return this
+  }
+
   // Get bar class
   barClass(d) {
     return `bar bar-${slugify(
@@ -645,7 +745,57 @@ class BarVerticalChart extends Chart {
   }
 }
 
+class TooltipStacked extends Tooltip {
+  setupLabels() {
+    // get a copy of keys array to reverse it
+    let keys = this.chart.data.keys.slice(0);
+    keys.reverse().forEach(key => {
+      this.el.append('div').attr('class', `label ${slugify(key.toLowerCase())}`);
+    });
+    return this
+  }
+
+  setContent(data) {
+    // Set key label
+    this.setLabel('x', this.chart.tooltipFormatX()(data[this.chart.key]));
+    // Set other keys labels
+    this.chart.data.keys.forEach(key => {
+      this.setLabel(
+        slugify(key.toLowerCase()),
+        this.chart.tooltipFormatY()(data[key]),
+        data[key] > 0 // show/hide label based on value
+      );
+    });
+  }
+
+  setPosition(dataX) {
+    const { x, y } = this.chart.getDataPosition(dataX);
+    // set x position
+    this.setPositionX(
+      x,
+      this.config.align !== 'auto'
+        ? this.config.align
+        : x > this.chart.width / 2
+        ? 'right'
+        : 'left'
+    );
+    // set y position for each key label
+    d3Collection.keys(y).forEach(key => {
+      this.el
+        .select(`.label.${key}`)
+        .style('bottom', `${this.chart.height - y[key]}px`);
+    });
+    return this
+  }
+}
+
 class StackedBarVerticalChart extends BarVerticalChart {
+  setup(data, key) {
+    this.key = key;
+    super.setup(data);
+    return this
+  }
+
   // Set scales
   setScales() {
     super.setScales();
@@ -657,25 +807,22 @@ class StackedBarVerticalChart extends BarVerticalChart {
     return this
   }
 
-  setup(data, key) {
-    this.data = this.getDataStack(data, key);
-    this.setFormat();
-    this.onResize();
-    this.setScales();
-    this.setAxis();
-    this.setRenderer();
-    return this
+  setTooltip() {
+    this.tooltip = new TooltipStacked(this, {
+      align: 'center',
+      valign: 'bottom'
+      //background: true
+    });
   }
 
-  getDataStack(data, key) {
-    const keys = Object.keys(data[0]).filter(d => d !== key);
-    const dataStacked = d3Shape.stack()
+  setData(data) {
+    const keys = Object.keys(data[0]).filter(d => d !== this.key);
+    this.data = d3Shape.stack()
       .keys(keys)
       .order(d3Shape.stackOrderNone)
       .offset(d3Shape.stackOffsetNone)(data);
-    dataStacked.keys = keys;
-    dataStacked.values = data.map(d => d[key]);
-    return dataStacked
+    this.data.keys = keys;
+    this.data.values = data.map(d => d[this.key]);
   }
 
   // Render chart bars
@@ -687,12 +834,14 @@ class StackedBarVerticalChart extends BarVerticalChart {
       .enter()
       .append('g')
       .attr('fill', (d, i) => this.scaleColor(this.data.keys[i]))
-      .attr('class', this.barClass.bind(this))
+      .attr('class', this.barClass.bind(this));
+    // add stacked bars items
+    this.bars
       .selectAll('.bar-stack-item')
       .data(d => d)
       .enter()
       .append('rect')
-      .attr('class', 'bar-stack-item')
+      .attr('class', this.barItemClass.bind(this))
       .call(this.setBarDimensions.bind(this));
     return this
   }
@@ -719,9 +868,61 @@ class StackedBarVerticalChart extends BarVerticalChart {
     return [0, d3Array.max(this.data, d => d3Array.max(d, e => d3Array.max(e)))]
   }
 
+  // Tooltip data for a mouse position
+  getMouseData(x) {
+    // get mouse position
+    const mouseX = this.scaleX.invert(x);
+    // get x index from data values
+    const i = this.data.values.indexOf(mouseX);
+    // get data for current index
+    return this.data[0][i].data
+  }
+
+  // Tooltip position for a given data
+  getDataPosition(data) {
+    // get current data x value
+    const x = data[this.key];
+    // get x position for current data
+    const obj = {
+      x: this.scaleX(x) + this.scaleX.bandwidth() / 2,
+      y: {}
+    };
+    // get x data index
+    const index = this.data.values.indexOf(x);
+    // get y position for each key
+    this.data.keys.forEach((key, i) => {
+      if (data[key]) {
+        const y = this.data[i][index];
+        obj.y[slugify(key.toLowerCase())] = this.scaleY((y[0] + y[1]) / 2);
+      }
+    });
+    return obj
+  }
+
+  // Mouse over chart highlight
+  highlight(data) {
+    // clear highlighted x axis labels
+    this.el.selectAll('.axis.x text').classed('active', false);
+    // clear highlighted bars
+    this.el.selectAll('.bar-stack-item').classed('active', false);
+    if (data) {
+      // highlight current x axis label
+      this.el
+        .selectAll('.axis.x text')
+        .filter(d => d === data[this.key])
+        .classed('active', true);
+      // highlight current bar
+      this.el.select(`.bar-${data[this.key]}`).classed('active', true);
+    }
+    return this
+  }
+
   // Get bar class
   barClass(d) {
     return `bar-stack bar-${slugify(d.key.toLowerCase())}`
+  }
+  barItemClass(d) {
+    return `bar-stack-item bar-${d.data[this.key]}`
   }
 
   // Set scale color for keys
@@ -865,12 +1066,34 @@ class TreemapChart extends Chart {
   }
 }
 
+class Legend {
+  constructor(selector) {
+    // Select legend container
+    this.el = d3Selection.select(selector)
+      .append('div')
+      .attr('class', 'legend');
+  }
+
+  setup(data, colors) {
+    // append labels to legend element
+    // get a copy of keys array to reverse it
+    data.forEach(key => {
+      this.el
+        .append('div')
+        .attr('class', `legend-label ${slugify(key.toLowerCase())}`)
+        .html(`<span style="background: ${colors(key)}"></span> ${key}`);
+    });
+    return this
+  }
+}
+
 exports.Chart = Chart;
 exports.AreaChart = AreaChart;
 exports.BarVerticalChart = BarVerticalChart;
 exports.LineChart = LineChart;
 exports.StackedBarVerticalChart = StackedBarVerticalChart;
 exports.TreemapChart = TreemapChart;
+exports.Legend = Legend;
 exports.Tooltip = Tooltip;
 
 Object.defineProperty(exports, '__esModule', { value: true });
