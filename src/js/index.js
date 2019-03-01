0e803461230f422633cca5065902baf8e7940c2f
Raúl Díaz Poblete <raul.lete@gmail.com>
2019-03-01 13:18:08 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,12 @@ export { default as AreaChart } from './areaChart'
 export { default as BarHorizontalChart } from './barHorizontalChart'
 export { default as BarVerticalChart } from './barVerticalChart'
 export { default as LineChart } from './lineChart'
+export {
+  default as StackedBarHorizontalChart
+} from './stackedBarHorizontalChart'
 export { default as StackedBarVerticalChart } from './stackedBarVerticalChart'
 export { default as TreemapChart } from './treemapChart'
+export { default as Table } from './table'
 export { default as Legend } from './legend'
 export { default as Tooltip } from './tooltip'
 export { formatDefaultLocale, timeFormatDefaultLocale } from './locales'
